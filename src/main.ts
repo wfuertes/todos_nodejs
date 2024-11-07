6420299add688255bcf621389c902b4076b5dd6b
Willian Batista <willian.batista@justeattakeaway.com>
2024-11-07 13:42:04 -0700

    Add pino logger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import pinoHttp from 'pino-http';
 import { db } from '@db';
 import { todosRoutes, MySqlTodoRepository } from '@todos';
 
+const pino = pinoHttp();
+
 const app = express();
 app.use(express.json());
+app.use(pino);
 
 // Repositories
 const todoRepository = new MySqlTodoRepository(db);
@@ -14,5 +18,5 @@ todosRoutes(app, todoRepository);
 const port = 8080;
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    pino.logger.info(`Server running at http://localhost:${port}`);
 });
